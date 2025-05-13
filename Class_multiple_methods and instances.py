de0b1dfc33ae2f3004656abdda825f8a62195e49
Keerthana2701 <keerthanapunithan@gmail.com>
2025-05-13 12:21:13 -0500

    adding new class file

--- Class_multiple_methods and instances.py
+++ Class_multiple_methods and instances.py
@@ -1,19 +1,30 @@
 print('class with multiple instances')
-class student:
-    def __init__(self,no,name,mark1,mark2):
-        self.no=no
-        self.name=name
-        self.mark1=mark1
-        self.mark2=mark2
+
+class Student:
+    def __init__(self, no, name, mark1, mark2):
+        self.no = no
+        self.name = name
+        self.mark1 = mark1
+        self.mark2 = mark2
+
     def studentname(self):
-        print('student name:{self.name}')
-    def studentmarks(self,mark1,mark2):
-        print("student total mark:{mark1+mark2}")
-    def studentdetails(self,no,name,mark1,mark2):
-        print("student details: \n name:{self.name}\n number:{self.no}\n mark1 :{self.mark1} \n mark2:{self.mark2}" )
+        print(f'Student name: {self.name}')
 
-#create 2 instances
-S1=student('100','keerthu',200,600)
-S2=student('111','Vikee',500,500)
+    def studentmarks(self):
+        total = self.mark1 + self.mark2
+        print(f"Student total mark: {total}")
+
+    def studentdetails(self):
+        print(f"Student details:\n Name: {self.name}\n Number: {self.no}\n Mark1: {self.mark1}\n Mark2: {self.mark2}")
+
+# Create 2 instances
+S1 = Student('100', 'Keerthu', 200, 600)
+S2 = Student('111', 'Vikee', 500, 500)
+
+
+# Call methods
 S1.studentdetails()
-
+S1.studentmarks()
+S2.studentdetails()
+S2.studentmarks()
+S2.studentname()
