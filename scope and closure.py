51760aff86dd0b4ce01419dc02bac8fdc327b0cb
Keerthana2701 <keerthanapunithan@gmail.com>
2025-05-14 13:00:31 -0500

    scope closure codes

---
+++ scope and closure.py
@@ -0,0 +1,29 @@
+#scope
+name = "keer"
+
+def outer():
+    name = "vikee"
+
+    def inner():
+        name = "aadhvik"
+        print("Inner:", name)
+
+    inner()
+    print("Outer:", name)
+
+outer()
+print("Global:", name)
+
+#closure
+#defined inside another function
+#has access to parent function's
+# variable even afer parent function is executed
+
+def make_multiplier(n):
+    def multiply(x):
+        return n * x
+    return multiply
+
+
+var = make_multiplier(7)
+print(var(5))
